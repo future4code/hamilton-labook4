54716c51a4f5da487c80e7fab5eee1a9612eaf46
MarioLuzz <marionetoluz@hotmail.com>
2020-06-25 15:12:36 -0300

    atualização show

---
+++ src/business/CommentBusiness.ts
@@ -0,0 +1,24 @@
+import { Comment } from "../models/Comments";
+import { CommentDatabase } from "../data/CommentDatabase";
+import { PostsDatabase } from "../data/PostDatabase";
+
+export class CommentBusiness {
+  async createComment(
+      comment: string,
+      userId: string,
+      postId: string,
+      commentId: string
+  ) {
+    const verifyPostId = await new PostsDatabase().verifyPostId(postId);
+
+    if (!verifyPostId) {
+      throw new Error(" Post ID Invalido!");
+    }
+
+    const commentData = new Comment(commentId, comment, userId, postId);
+
+    const commentDatabase = new CommentDatabase();
+
+    await commentDatabase.createComment(commentData);
+  }
+}
